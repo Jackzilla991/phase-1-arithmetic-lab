06fdf112a47277d0e44970cb9f3702512bc35c6b
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-14 19:30:03 -0500

    commit tree 7eca7ec4fe69bf8d1be4eabc0a002c9835a24be6

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 4;
+const num4 = 17;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100);
+console.log(random);
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(5, 10, 15, 20);
+console.log(max);
